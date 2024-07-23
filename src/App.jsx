cde6338cc73a8d17fa09fba759d3362fb5b706b9
Kachii <kachikaduru@gmail.com>
2024-07-23 22:41:54 +0100

    React Router
    Installed the React Router package and created some routes to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-export default function App() {
-  // const x = 23;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
 
-  return <h1>Worldwise</h1>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
