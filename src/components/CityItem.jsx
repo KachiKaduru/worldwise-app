9f5ca5ae3e155da1bac14145179e9cbdc9a0e355
Kachii <kachikaduru@gmail.com>
2024-07-29 13:13:42 +0100

    New changes
    - created the currentCity state and getCity function to get the currently clicked
    city, set it, and store it in a variable, currentCity.  I then passed them as values
    into the CityContext.Provider component.
    - passed getCity and currentCity into the City component. Got the id and used that
    to load the currently clicked city (currentCity).
    - used the currentCity to set a class on the currently clicked city.
    - created a reuseable BackButton component and added it to both the Form component
    and the City component.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -12,9 +13,14 @@ const formatDate = (date) =>
 export default function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
 
+  const { currentCity } = useCities();
+
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        className={`${styles.cityItem} ${id === currentCity.id && styles["cityItem--active"]}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time>({formatDate(date)})</time>
