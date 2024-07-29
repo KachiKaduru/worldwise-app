5883986d69e1542ce505815d197c52ae9c812255
Kachii <kachikaduru@gmail.com>
2024-07-29 11:44:43 +0100

    Integrated the Context API into this App
    Created a seperate folder for contexts and a seperate file for CitiesContext
    to use the Context API for better state management for this application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -12,50 +12,31 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 
-const BASE_URL = "http://localhost:8000/";
-
 export default function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("Problem occured loading the data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* PAGES ROUTES  */}
-        <Route index path="/" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="login" element={<Login />} />
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* PAGES ROUTES  */}
+          <Route index path="/" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
 
-        {/* NESTED ROUTES */}
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to={"cities"} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          {/* STORING STATE IN URL ROUTE */}
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<Form />} />
-        </Route>
+          {/* NESTED ROUTES */}
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to={"cities"} />} />
+            <Route path="cities" element={<CityList />} />
+            {/* STORING STATE IN URL ROUTE */}
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
 
-        {/* INCASE THE PAGE DOES NOT EXIST  */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+          {/* INCASE THE PAGE DOES NOT EXIST  */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
