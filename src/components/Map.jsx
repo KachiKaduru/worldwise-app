392a486b61409355b4822aff149db1c3744b1b48
Kachii <kachikaduru@gmail.com>
2024-07-31 23:05:05 +0100

    Setting Map Possition with Geolocation Position
    here, i had a function ofr getting the Geolocation of a user and then implemented it
    in this map, by using it as a hook, then calling it in the Map component.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,13 +3,18 @@ import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "re
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 export default function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -20,8 +25,20 @@ export default function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type={"position"} onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         // center={[mapLat, mapLng]}
